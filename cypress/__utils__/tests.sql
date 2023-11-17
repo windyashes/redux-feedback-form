19cd387c9964fcfa0dd7b4bd2111dc6fd9f10d94
Dane Smith <doctorhowser@gmail.com>
2023-11-17 13:35:58 -0600

    stub out potential tests

--- cypress/__utils__/tests.sql
+++ cypress/__utils__/tests.sql
@@ -1,15 +1,16 @@
-DROP TABLE IF EXISTS "gallery";
+DROP TABLE IF EXISTS "feedback";
 
 
-CREATE TABLE "gallery" (
-    "id" SERIAL PRIMARY KEY,
-    "url" VARCHAR,
-    "title" VARCHAR,
-    "description" TEXT,
-    "likes" INTEGER DEFAULT 0
-);
+CREATE TABLE "feedback" (
+  "id" serial primary key,
+  "feeling" INT not null,
+  "understanding" INT not null,
+  "support" INT not null,
+  "comments" text,
+  "flagged" boolean default false,
+  "date" date not null default CURRENT_DATE
+); 
 
-INSERT INTO "gallery" ("url", "title", "description", "likes")
-VALUES ('images/goat_small.jpg', 'Goat!', 'Photo of a goat taken at Glacier National Park.', 2),
-         ('images/goat_small.jpg', 'Different...', 'Not the same goat!', 1),
-         ('images/goat_small.jpg', 'Something Else!', 'Not at all the same!', 0);
+-- Sample feedback item
+INSERT INTO "feedback" ("feeling", "understanding", "support", "comments")
+VALUES (4, 4, 5, 'Doing Great!');
--- cypress/e2e/gallery.cy.js
+++ cypress/e2e/gallery.cy.js
@@ -1,4 +1,4 @@
-describe('Gallery of My Life', () => {
+describe('Feedback Loop', () => {
   beforeEach(async () => {
     // Silence console.log statements for cleaner test output:
     console.log = () => { }
@@ -8,77 +8,22 @@ describe('Gallery of My Life', () => {
     await cy.visit('/')
   })
 
-  // Verify that the app renders the two initial to-do items
-  // that were provided in database.sql:
-  it('READ: Displays initial Gallery items upon load', () => {
-    // Test that three elements with a data-testid value of "toDoItem" exist:
-    cy.get('[data-testid="app"]')
-      .should('have.length', 1)
 
-    cy.get('[data-testid="galleryList"]')
-      .should('have.length', 1)
-
-    cy.get('[data-testid="galleryItem"]')
-      .should('have.length', 3)
-
-  })
-
-  it('UI: Item Toggles between image and description', () => {
-    // Test that the 'Photo of a goat taken at Glacier National Park.' Picture is visible
-    cy.contains('[data-testid="galleryItem"]', 'Goat!')
-      .find('img')
-      .should('be.visible');
-
-    //click image to show description
-
-
+  it('UI: Correct order of views', () => {
   
-    cy.contains('[data-testid="galleryItem"]', 'Goat!')
-      .find('[data-testid="toggle"]')
-      .click();
-
-
-    cy.contains('[data-testid="galleryItem"]', 'Photo of a goat taken at Glacier National Park.').should('be.visible')
-    //making sure not all of the images get toggled
-    cy.contains('[data-testid="galleryItem"]', 'Different...').find('img').should('be.visible');
-
-    //Trying to account for if they chose display=none instead of swapping elements
-    //Def gross, but I don't know how to do it better
-    cy.contains('[data-testid="galleryItem"]', 'Goat!').then(item => {
-      if (item.find("img").length > 0) {
-        cy.contains('[data-testid="galleryItem"]', 'Goat!').find("img").should("not.be.visible");
-      } else {
-        cy.contains('[data-testid="galleryItem"]', 'Goat!').find("img").should("not.exist");
-      }
-    });
-
-
-    //click description to show image
-    cy.contains('[data-testid="galleryItem"]', 'Photo of a goat taken at Glacier National Park.').find('[data-testid="toggle"]').click()
-
-    cy.contains('[data-testid="galleryItem"]', 'Goat!').find('img').should('be.visible');
   })
 
-  // Verify that the app behaves correctly when two to-do
-  // items have their "complete" buttons clicked:
-  it('UPDATE: It can add likes to the database', () => {
-    // Test that the 'Photo of a goat taken at Glacier National Park.' Picture has 2 likes
-    cy.contains('[data-testid="galleryItem"]', 'Goat!').contains(2).should('exist');
+  it('UI: Data Collection Persists across views', () => {
+  
+  })
 
-    cy.contains('[data-testid="galleryItem"]', 'Different...').contains(1).should('exist');
+  //This test is to ensure they didnt break anything...?
+  it('GET: Able to retrieve Data', () => {
+  
+  })
 
-    cy.contains('[data-testid="galleryItem"]', 'Goat!').find('[data-testid="like"]').click();
 
-
-    cy.contains('[data-testid="galleryItem"]', 'Goat!').contains(3).should('exist');
-
-    cy.contains('[data-testid="galleryItem"]', 'Different...').contains(1).should('exist');
+  it('POST: Adds feedback to Database', () => {
     
-    //reload the page
-    cy.reload();
-    cy.contains('[data-testid="galleryItem"]', 'Goat!').contains(3).should('exist');
-
-    cy.contains('[data-testid="galleryItem"]', 'Different...').contains(1).should('exist');
-
   })
 })
