dd1bb586b9fe0d9d89b3e107d8dfa4477245dd9c
Dane Smith <doctorhowser@gmail.com>
2023-11-29 15:59:29 -0600

    add fix

--- cypress/e2e/gallery.cy.js
+++ cypress/e2e/gallery.cy.js
@@ -19,25 +19,25 @@ describe('Feedback Loop', () => {
     //will cycle through the 6 views and check they are what we expect
     // based on finding 'correct' text on that view
     // 🚧 If you add input validation to your project, this test will need updating. 
-    cy.get('feeling').should('exist');
+    cy.contains('feeling').should('exist');
     cy.get('[data-testid="next"]').click();
 
-    cy.get('understanding').should('exist');
+    cy.contains('understanding').should('exist');
     cy.get('[data-testid="next"]').click();
 
-    cy.get('support').should('exist');
+    cy.contains('support').should('exist');
     cy.get('[data-testid="next"]').click();
 
-    cy.get('comments').should('exist');
+    cy.contains('comments').should('exist');
     cy.get('[data-testid="next"]').click();
 
-    cy.get('review').should('exist');
+    cy.contains('review').should('exist');
     cy.get('[data-testid="next"]').click();
 
-    cy.get('thank').should('exist');
+    cy.contains('thank').should('exist');
     cy.get('[data-testid="next"]').click();
 
-    cy.get('feeling').should('exist');
+    cy.contains('feeling').should('exist');
   })
 
   it('UI: Data Collection Persists across views, sends Collected Data', async () => {
@@ -46,35 +46,35 @@ describe('Feedback Loop', () => {
 
     //will cycle through the 6 views and check they are what we expect
     // based on finding 'correct' text on that view
-    cy.get('feeling').should('exist');
+    cy.contains('feeling').should('exist');
     cy.get('[data-testid="input"]').type('5')
 
     cy.get('[data-testid="next"]').click();
 
-    cy.get('understanding').should('exist');
+    cy.contains('understanding').should('exist');
     cy.get('[data-testid="input"]').type('4')
 
     cy.get('[data-testid="next"]').click();
 
-    cy.get('support').should('exist');
+    cy.contains('support').should('exist');
     cy.get('[data-testid="input"]').type('2')
 
     cy.get('[data-testid="next"]').click();
 
-    cy.get('comments').should('exist');
+    cy.contains('comments').should('exist');
     cy.get('[data-testid="input"]').type('Taco Cat Goat Cheese Pizza')
 
     cy.get('[data-testid="next"]').click();
 
-    cy.get('review').should('exist');
+    cy.contains('review').should('exist');
 
 
-    cy.get('Taco Cat Goat Cheese Pizza').should('exist');
-    cy.get('5').should('exist')
-    cy.get('4').should('exist')
-    cy.get('2').should('exist')
-    cy.get('3').should('not.exist')
-    cy.get('1').should('not.exist')
+    cy.gcontainset('Taco Cat Goat Cheese Pizza').should('exist');
+    cy.contains('5').should('exist')
+    cy.contains('4').should('exist')
+    cy.contains('2').should('exist')
+    cy.contains('3').should('not.exist')
+    cy.contains('1').should('not.exist')
 
     cy.wait('@postRequest').its('body').should('contain', '')
 
@@ -84,30 +84,30 @@ describe('Feedback Loop', () => {
 
 
   it('POST: Adds feedback to Database', () => {
-    cy.get('feeling').should('exist');
+    cy.contains('feeling').should('exist');
     cy.get('[data-testid="input"]').type('5')
 
     cy.get('[data-testid="next"]').click();
 
-    cy.get('understanding').should('exist');
+    cy.contains('understanding').should('exist');
     cy.get('[data-testid="input"]').type('4')
 
     cy.get('[data-testid="next"]').click();
 
-    cy.get('support').should('exist');
+    cy.contains('support').should('exist');
     cy.get('[data-testid="input"]').type('2')
 
     cy.get('[data-testid="next"]').click();
 
-    cy.get('comments').should('exist');
+    cy.contains('comments').should('exist');
     cy.get('[data-testid="input"]').type('Taco Cat Goat Cheese Pizza')
 
     cy.get('[data-testid="next"]').click();
 
-    cy.get('review').should('exist');
+    cy.contains('review').should('exist');
 
 
-    cy.get('Taco Cat Goat Cheese Pizza').should('exist');
+    cy.contains('Taco Cat Goat Cheese Pizza').should('exist');
     cy.get('[data-testid="next"]').click();
 
 
