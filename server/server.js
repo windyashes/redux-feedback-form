1377432e9c5c70beb2b08059703db1c09cdd3fec
Dane Smith <doctorhowser@gmail.com>
2023-12-28 14:44:59 -0600

    migrate from CRA to Vite via script

--- server/server.js
+++ server/server.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5001;
 const feedbackRouter = require('./routes/feedback.router')
 
 /** ---------- MIDDLEWARE ---------- **/
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json()); 
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
-app.use('/feedback', feedbackRouter)
+app.use('/api/feedback', feedbackRouter)
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
