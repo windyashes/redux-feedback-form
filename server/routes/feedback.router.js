72a8020883b7e4c80b1696361aba59bcdd0cc496
Marc McCarthy <marstheory20@gmail.com>
2023-12-29 15:06:01 -0600

    remove unneeded files, correct name, formatting

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const pool = require('../modules/pool')
 
 
-
 // TODO: This route adds a new feedback entry
 router.post('/', (req, res) => {
 
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-    import { defineConfig } from 'vite';
-    import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-    export default defineConfig(() => {
-        return {
-            build: {
-                outDir: 'build',
-            },
-            server: {
-                proxy: {
-                    '/api':'http://localhost:5001',
-                }
-            },
-            plugins: [react()],
-        };
-    });
+export default defineConfig(() => {
+    return {
+        build: {
+            outDir: 'build',
+        },
+        server: {
+            proxy: {
+                '/api':'http://localhost:5001',
+            }
+        },
+        plugins: [react()],
+    };
+});
